9e8eea339a85ef4dd6a5d81a638209165467d2f7
patchangg <patrrynnn@hotmail.com>
2021-07-11 20:18:47 +1000

    Daily Leetcode

---
+++ Python/Medium/912.py
@@ -0,0 +1,8 @@
+
+def sortArray(nums):
+    return sorted(nums)
+
+# Use sorted to sort the integer array. O(n log n), O(1) space
+nums = [5,1,1,2,0,0]
+sortedArray = sortArray(nums)
+print(sortedArray)
