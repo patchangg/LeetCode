d2368c91d0fc4134168df57f5b9b9eee567ad43c
patchangg <patrrynnn@hotmail.com>
2021-08-15 17:54:45 +1000

    Update 1016.py

--- Python/Medium/1016.py
+++ Python/Medium/1016.py
@@ -1,9 +1,13 @@
 
-def queryString(self, s: str, n: int) -> bool:
+def queryString(s, n):
     for i in range(n,0,-1):
         if bin(i)[2:] not in s:
             return False
     return True
 
+s = "0110"
+n = 3
+query = queryString(s,n)
+print(query)
 # Check every binary representation from n to 0 is in n
 # O(slogn), O(s) space
