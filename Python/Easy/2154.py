75dc7676c062dbe804701c6cc7aa0a608c605da9
patchangg <patrrynnn@hotmail.com>
2022-07-01 22:07:39 +1000

    Daily Leetcode

---
+++ Python/Easy/2154.py
@@ -0,0 +1,10 @@
+
+def findFinalValue(nums, original):
+    while original in nums:
+        original *= 2
+    return original
+
+nums = [5,3,6,1,12]
+original = 3
+finalValue = findFinalValue(nums,original)
+print(finalValue)
