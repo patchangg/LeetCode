e7ae3495c2e8e35a6523c8f502824489caf2c79b
patchangg <patrrynnn@hotmail.com>
2022-11-30 20:33:06 +1100

    Daily Leetcode

---
+++ Python/Easy/703.py
@@ -0,0 +1,23 @@
+import heapq
+
+class KthLargest:
+
+    def __init__(self, k, nums):
+        self.k = k
+        self.heap = nums
+        heapq.heapify(self.heap)
+        while len(self.heap) > k:
+            heapq.heappop(self.heap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap,val)
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
+
+kthLargest = KthLargest(3, [4, 5, 8, 2])
+print(kthLargest.add(3))
+print(kthLargest.add(5))
+print(kthLargest.add(10))
+print(kthLargest.add(9))
+print(kthLargest.add(4))
