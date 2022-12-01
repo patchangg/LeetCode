c178c3e871351f7cdc6fd5bb3fb69b63d07af602
patchangg <patrrynnn@hotmail.com>
2022-12-01 23:48:54 +1100

    Daily Leetcode

--- Python/Easy/2335.py
+++ Python/Easy/2335.py
@@ -1,3 +1,20 @@
 
-def sum(num1, num2):
-    return num1 + num2
+def fillCups(amount):
+    output = 0
+    amount = sorted(amount)
+
+    while amount[2] > 0 and amount[1] > 0:
+        amount[2] -= 1
+        amount[1] -= 1
+        output += 1
+        amount = sorted(amount)
+
+    while amount[2] > 0:
+        amount[2] -= 1
+        output += 1
+
+    return output
+
+amount = [1,4,2]
+minTimeToFill = fillCups(amount)
+print(minTimeToFill)
